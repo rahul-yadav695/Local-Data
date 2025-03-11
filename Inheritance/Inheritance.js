7806034e3792126811e9638d48646f362497d2d0
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-03-11 18:39:38 +0530

    new folder edit

---
+++ Inheritance/Inheritance.js
@@ -0,0 +1,21 @@
+class Car {
+    constructor(brand) {
+      this.carname = brand;
+    }
+    present() {
+        console.log(`this car uses ${this.carname}`)
+    }
+  }
+  
+  class Model extends Car {
+    constructor(brand, mod) {
+      super(brand);
+      this.model = mod;
+    }
+    show() { 
+    console.log(`${this.present()} it a ${this.model}`)
+    }
+  }
+  
+  let myCar = new Model("Ford", "Mustang");
+  document.getElementById("demo").innerHTML = myCar.show();
