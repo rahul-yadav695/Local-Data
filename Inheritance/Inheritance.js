ca558250faf1b06ea781b6d1b947ddcd7209d64d
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-03-12 09:46:12 +0530

    now edit

--- Inheritance/Inheritance.js
+++ Inheritance/Inheritance.js
@@ -1,21 +1,24 @@
 class Car {
-    constructor(brand) {
-      this.carname = brand;
-    }
-    present() {
-        console.log(`this car uses ${this.carname}`)
-    }
+  constructor(brand) {
+    this.carname = brand;
   }
-  
-  class Model extends Car {
-    constructor(brand, mod) {
-      super(brand);
-      this.model = mod;
-    }
-    show() { 
+  present() {
+    console.log(`this car uses ${this.carname}`)
+  }
+}
+
+class Model extends Car {
+  constructor(brand, mod) {
+    super(brand);
+    this.model = mod;
+  }
+  show() {
     console.log(`${this.present()} it a ${this.model}`)
-    }
   }
-  
-  let myCar = new Model("Ford", "Mustang");
-  document.getElementById("demo").innerHTML = myCar.show();
+}
+
+let myCar = new Model("Ford", "Mustang");
+console.log(myCar)
+// document.getElementById("demo").innerHTML = myCar.show();
+
+
