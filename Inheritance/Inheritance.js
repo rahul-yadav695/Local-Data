e1660062546e571ea6cfd9d3e2a164761a9922de
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-03-24 18:01:07 +0530

    edit

--- Inheritance/Inheritance.js
+++ Inheritance/Inheritance.js
@@ -384,8 +384,8 @@ for (let i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i]))
         for (let j = 0; j < arr[i]; j++) {
             newarr.push(arr[i][j])
-        }else{
-            newarr.push(arr[i])
-        }
+        } else {
+        newarr.push(arr[i])
+    }
 }
 console.log(newarr)
