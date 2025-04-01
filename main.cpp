b28970ff4fb669ab4fc8b29ce28842e32fef64c2
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-04-01 12:38:31 +0530

    add

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
-using namespace std ; 
-int main (){
-    int arr[] = {15,65,98,78,24,12,2};
-    int max = arr[0];
-    int size = sizeof(arr) / sizeof(arr[0]);
-    for(int i=1; i<size; i++){
-      if(arr[i]>max){
-        max = arr[i];
-      }
-      cout<<max;
+// #include <iostream>
+// using namespace std ; 
+// int main (){
+//     int arr[] = {15,65,98,78,24,12,2};
+//     int max = arr[0];
+//     int size = sizeof(arr) / sizeof(arr[0]);
+//     for(int i=1; i<size; i++){
+//       if(arr[i]>max){
+//         max = arr[i];
+//       }
+//       cout<<max;
 
-      return 0 ;
-    }
-}
+//       return 0 ;
+//     }
+// }
+
