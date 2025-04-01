3509b4ea14aaa4d885d0bf5cf8d84609095c9a12
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-04-01 12:50:39 +0530

    edit

--- main.cpp
+++ main.cpp
@@ -1,39 +1,46 @@
-// #include <iostream>
-// using namespace std ; 
-// int main (){
-//     int arr[] = {15,65,98,78,24,12,2};
-//     int max = arr[0];
-//     int size = sizeof(arr) / sizeof(arr[0]);
-//     for(int i=1; i<size; i++){
-//       if(arr[i]>max){
-//         max = arr[i];
-//       }
-//       cout<<max;
-
-//       return 0 ;
-//     }
-// }
-
 #include <iostream>
-using namespace std; 
+using namespace std ; 
+int main (){
+    int arr[] = {15,65,98,78,24,12,2};
+    int max = arr[0];
+    int size = sizeof(arr) / sizeof(arr[0]);
+    // for(int i=1; i<size; i++){
+    //   if(arr[i]>max){
+    //     max = arr[i];
+    //   }
+    //   cout<<max;
+    // }
+    //   return 0 ;
 
-int main() {
-    int arr[] = {15, 65, 98, 78, 24, 12, 2};
-    int max = arr[0]; 
-    int size = sizeof(arr) / sizeof(arr[0]);  
-
-    for (int i = 1; i < size; i++) {  
-        if (arr[i] > max) {
-            max = arr[i];  
+    for(int i = 1; i<size ; i++){
+        if(arr[i]>max){
+            max = arr[i]
         }
+        cout<<max
     }
-
-    // cout << max;  
-
-    return 0; 
-
-    // const double value = 10
-    // int pi = 3.14
-    // int cricle = 2*value*pi
-    // cout<<cricle
+    
 }
+
+// #include <iostream>
+// using namespace std; 
+
+// int main() {
+//     int arr[] = {15, 65, 98, 78, 24, 12, 2};
+//     int max = arr[0]; 
+//     int size = sizeof(arr) / sizeof(arr[0]);  
+
+//     for (int i = 1; i < size; i++) {  
+//         if (arr[i] > max) {
+//             max = arr[i];  
+//         }
+//     }
+
+//     // cout << max;  
+
+//     return 0; 
+
+//     // const double value = 10
+//     // int pi = 3.14
+//     // int cricle = 2*value*pi
+//     // cout<<cricle
+// }
