bc12243bb11f822d7997f2664e6961f15acb593b
rahul-yadav695 <officialrahulyadav961@gmail.com>
2025-04-10 18:28:20 +0530

    comment

--- Log in/script.js
+++ Log in/script.js
@@ -164,7 +164,7 @@ if (passwordValue.length <= 10) {
 
 
 
-let arr = [1, 2, [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
+// let arr = [1, 2, [1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]];
 let newarr = [];
 for (let i = 0; i < arr.length; i++) {
   if (Array.isArray(arr[i])) {
